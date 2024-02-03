10d41e945806c1d079cac8791eced66fc6716ec0
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-02-04 01:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const grid = document.querySelector(".grid-container");
+const reset = document.getElementById("Reset");
+const form = document.getElementById("form");
+let previousSelectedElement = null;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let cellId = form.inputId.value;
+  let cell = document.getElementById(cellId);
+  if(previousSelectedElement){
+    previousSelectedElement.style.backgroundColor="white";
+	}
+  previousSelectedElement = cell;
+  let color = form.color.value;
+  cell.style.backgroundColor = color;
+  form.reset();
+});
+
+for(let i=1 ; i<=9 ; i++){
+  const gridItem = document.createElement("div");
+  gridItem.id = i;
+  gridItem.innerText = i;
+  gridItem.className = "grid-item";
+  grid.appendChild(gridItem);
+}
+reset.addEventListener("click" , (e) => {
+	for(let i=1 ; i<=9 ; i++){
+		let cell = document.getElementById(i);
+		previousSelectedElement = null;
+  		cell.style.backgroundColor = "white";
+	}
+});
